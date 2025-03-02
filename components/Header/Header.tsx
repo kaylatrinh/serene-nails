e79cf4aaddd0c9bdb77b2d18ff5c1e3435f936b5
kaylatrinh <kayla.trinh.nguyen@gmail.com>
2025-03-02 14:34:40 -0500

    style fixes based off input from client

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,22 +2,22 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <header className="bg-rose py-4">
+    <header className="bg-white py-4">
       <nav className="container mx-auto px-4 flex justify-between items-center">
         <div></div>
         <ul className="flex space-x-4">
           <li>
-            <Link href="#services" className="text-pink-600 hover:text-pink-800">
+            <Link href="#services" className="text-black hover:text-pink-800">
               Services
             </Link>
           </li>
           <li>
-            <Link href="#gallery" className="text-pink-600 hover:text-pink-800">
+            <Link href="#gallery" className="text-black hover:text-pink-800">
               Gallery
             </Link>
           </li>
           <li>
-            <Link href="#contact" className="text-pink-600 hover:text-pink-800">
+            <Link href="#contact" className="text-black hover:text-pink-800">
               Contact
             </Link>
           </li>
