ce102a8e278779ef3a7e60e17aa93d489b91e39b
kaylatrinh <kayla.trinh.nguyen@gmail.com>
2025-03-02 14:39:33 -0500

    development fixes

--- components/Welcome/Hero.tsx
+++ components/Welcome/Hero.tsx
@@ -1,25 +1,8 @@
-import Image from "next/image"
 import "./index.css"
-import SereneLogo from "@/public/logo/serene-logo.jpg";
 
 export default function Hero() {
   return (
-    <section className="relative h-[500px]">
-      {/*<Image*/}
-      {/*  src="/white-lotus/2.png"*/}
-      {/*  alt="Nail salon hero image"*/}
-      {/*  layout="fill"*/}
-      {/*  objectFit="cover"*/}
-      {/*  priority*/}
-      {/*/>*/}
-      {/*<div className="absolute inset-5 flex items-start">*/}
-      {/*    <Image*/}
-      {/*        src={SereneLogo}*/}
-      {/*        alt="Serene Logo"*/}
-      {/*        height={100}*/}
-      {/*        width={130}*/}
-      {/*    />*/}
-      {/*</div>*/}
+    <section className="relative h-[500px]">git a
       <div className="absolute inset-0 bg-green-900 bg-opacity-50 flex items-center justify-center">
           <div className="text-center">
               <h1 className="text-3xl md:text-5xl sm:text-5xl text-black border-black mb-4 alex-brush">Serene Nails</h1>
