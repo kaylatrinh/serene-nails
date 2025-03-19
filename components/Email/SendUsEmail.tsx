866f7af4bd18f25b590a8ca565409af9146e51f6
peter.nguyen <penguyen@gmail.com>
2025-03-19 12:51:15 -0700

    Encode apostrophe

--- components/Email/SendUsEmail.tsx
+++ components/Email/SendUsEmail.tsx
@@ -6,7 +6,7 @@ export default function SendUsEmail() {
         <div className="container mx-auto px-4">
             <h2 className="text-3xl font-bold text-center mb-8 text-pink-600">Send Us An Email</h2>
             <p className="text-center mb-8 text-gray-700">
-            Have questions or want to book an appointment? Fill out the form below and we'll get back to you!
+            Have questions or want to book an appointment? Fill out the form below and we&#39;ll get back to you!
             </p>
             <EmailForm />
         </div>
