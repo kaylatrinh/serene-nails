177db654714149fbb5188d8e02a216b4b6c60f22
kaylatrinh <kayla.trinh.nguyen@gmail.com>
2025-03-19 21:10:55 -0700

    minor style changes

--- components/Welcome/Hero.tsx
+++ components/Welcome/Hero.tsx
@@ -2,7 +2,7 @@ import "./index.css"
 
 export default function Hero() {
   return (
-    <section className="relative h-[500px]">
+    <section className="relative h-[600px]">
       <div className="absolute inset-0 bg-green-900 bg-opacity-50 flex items-center justify-center">
           <div className="text-center">
               <h1 className="text-5xl sm:text-7xl md:text-9xl lg:text-[200px]  text-black border-black mb-4 alex-brush">Serene Nails</h1>
