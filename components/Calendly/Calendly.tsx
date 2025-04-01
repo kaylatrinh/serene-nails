f99d1edcceef44d5272f164df02579001db6214f
kaylatrinh <kayla.trinh.nguyen@gmail.com>
2025-04-01 00:01:47 -0700

    changing widget height

--- components/Calendly/Calendly.tsx
+++ components/Calendly/Calendly.tsx
@@ -3,5 +3,6 @@
 import { InlineWidget } from "react-calendly";
 
 export default function CalendlyWidget() {
-    return <InlineWidget url="https://calendly.com/kayla-trinh-nguyen/30min" />;
+    return <InlineWidget url="https://calendly.com/kayla-trinh-nguyen/30min"
+                         styles={{ height: "700px" }} />;
 }
