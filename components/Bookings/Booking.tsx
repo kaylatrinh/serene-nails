bd690335850d494fa0dd1c0ac425944eef9632c5
kaylatrinh <kayla.trinh.nguyen@gmail.com>
2025-04-01 00:06:57 -0700

    changing viewport for mobile

--- components/Bookings/Booking.tsx
+++ components/Bookings/Booking.tsx
@@ -4,7 +4,7 @@ import CalendlyWidget from "@/components/Calendly/Calendly";
 export default function Booking() {
 
   return (
-      <div style={{overflow: "hidden"}}>
+      <div className="calendly-container">
           <h2 className="text-3xl font-bold text-center mb-8 text-green-950 text-opacity-75">Book With Us</h2>
           {/*<EmailForm></EmailForm>*/}
           <CalendlyWidget />
