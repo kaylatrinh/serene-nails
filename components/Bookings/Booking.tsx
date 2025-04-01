1bbcf30cac91a742e43c57cad874509d81b0abd6
kaylatrinh <kayla.trinh.nguyen@gmail.com>
2025-03-31 23:55:36 -0700

    implementing calendly

--- components/Bookings/Booking.tsx
+++ components/Bookings/Booking.tsx
@@ -1,12 +1,13 @@
-import EmailForm from "@/components/Email/EmailForm";
+// import EmailForm from "@/components/Email/EmailForm";
+import CalendlyWidget from "@/components/Calendly/Calendly";
 
 export default function Booking() {
 
   return (
-      <div id="booking">
-        <h2 className="text-3xl font-bold text-center mb-8 text-green-950 text-opacity-75">Book With Us</h2>
-        <EmailForm></EmailForm>
-      </div>
-  )
+      <div style={{overflow: "hidden"}}>
+          <h2 className="text-3xl font-bold text-center mb-8 text-green-950 text-opacity-75">Book With Us</h2>
+          {/*<EmailForm></EmailForm>*/}
+          <CalendlyWidget />
+      </div>)
 }
 
---
+++ components/Calendly/Calendly.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { InlineWidget } from "react-calendly";
+
+export default function CalendlyWidget() {
+    return <InlineWidget url="https://calendly.com/kayla-trinh-nguyen/30min" />;
+}
